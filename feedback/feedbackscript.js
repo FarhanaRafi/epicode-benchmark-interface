e07ec5dae367f63d684f68c6f8294103b31515ed
McNoHam14 <McNoHam14@gmail.com>
2022-12-12 15:52:13 +0000

    Mostly complete, stars still to finish

---
+++ feedback/feedbackscript.js
@@ -0,0 +1,58 @@
+
+function myFunction() {
+    var element = document.getElementById("uncheck");
+    element.classList.toggle("star-class")
+}
+
+
+
+/*
+star-class::before.click(function () {
+    starClassClick.toggleClass("star-class::after ");
+})
+*/
+
+
+
+
+/*
+window.onLoadActions()
+
+
+function onLoadActions () {
+    let star = 
+    for (let star = 0; star <=9; star ++) {
+    let starNode = document.createElement('stars-container')
+    starNode.innerHTML = star
+    starNode.classList.add('star-class')
+    document.getElementById('stars-container').appendChild(starNode)
+}
+3
+}
+
+
+function onStarClick (event) {
+    let clickedStarNode = event.target
+//    console.log(clickedNumberNode)
+clickedStarNode.classList.add('star-class')
+}
+*/
+
+
+// function to click selected stars
+
+
+
+// function to remove writing from input
+
+
+
+// more info button function event listener
+
+
+
+// function to display 6 selected stars using DOM
+
+
+
+// function to display 4 unselected stars using DOM
