77c886d2e7ce252e21446f062e1e6d224b3dbf02
McNoHam14 <McNoHam14@gmail.com>
2022-12-13 15:11:40 +0000

    Complete, small bug with star select

--- feedback/feedbackscript.js
+++ feedback/feedbackscript.js
@@ -1,58 +1,41 @@
+function onLoadActions(selectedStar) {
+    let starNode = document.getElementById('stars-container') 
+    starNode.innerHTML = ""
+    
+    for (let index = 0; index <selectedStar; index++) { // visualise 10 stars in container
+        let starNode = document.createElement('stars-container') // 1) change this to container ??
+        starNode.innerHTML = ""
+        let star = document.getElementById('star-svg').outerHTML // .onclick = selectStar();
 
-function myFunction() {
-    var element = document.getElementById("uncheck");
-    element.classList.toggle("star-class")
+        starNode.classList.add('star') // 2) customise star, add CSS class .star
+        starNode.innerHTML = star // assign innerHTML to star
+        starNode.addEventListener("click", onStarSelect)
+        starNode.id=index
+        document.getElementById('stars-container').appendChild(starNode)[index] // do i need [star] ?? // append it to HTML via the DOM, may need to change this ??
+    }
+
+    for (let index = selectedStar; index < 10; index++) { // visualise 10 stars in container
+        let starNode = document.createElement('stars-container') // 1) change this to container ??
+        let star = document.getElementById('star-svg-uncheck').outerHTML // .onclick = selectStar();
+
+
+        starNode.id = index + 1
+        // bug here, sometimes selects a lot of staras more than container //
+
+
+
+        starNode.classList.add('star') // 2) customise star, add CSS class .star
+        starNode.innerHTML = star // assign innerHTML to star
+        starNode.addEventListener("click", onStarSelect)
+
+        document.getElementById('stars-container').appendChild(starNode)[index] // do i need [star] ?? // append it to HTML via the DOM, may need to change this ??
+    }
 }
 
 
+window.onload = onLoadActions(3)
 
-/*
-star-class::before.click(function () {
-    starClassClick.toggleClass("star-class::after ");
-})
-*/
-
-
-
-
-/*
-window.onLoadActions()
-
-
-function onLoadActions () {
-    let star = 
-    for (let star = 0; star <=9; star ++) {
-    let starNode = document.createElement('stars-container')
-    starNode.innerHTML = star
-    starNode.classList.add('star-class')
-    document.getElementById('stars-container').appendChild(starNode)
+function onStarSelect(event){
+    console.log(event.currentTarget.id)
+    onLoadActions(event.currentTarget.id)
 }
-3
-}
-
-
-function onStarClick (event) {
-    let clickedStarNode = event.target
-//    console.log(clickedNumberNode)
-clickedStarNode.classList.add('star-class')
-}
-*/
-
-
-// function to click selected stars
-
-
-
-// function to remove writing from input
-
-
-
-// more info button function event listener
-
-
-
-// function to display 6 selected stars using DOM
-
-
-
-// function to display 4 unselected stars using DOM
